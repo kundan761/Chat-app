03ef90d1da21fecaf9f852901c8571b376fd407a
kundan761 <kundankumarsuraj761@gmail.com>
2024-08-09 00:23:44 +0530

    frontend is completed

--- frontend/src/components/ChatsPage.jsx
+++ frontend/src/components/ChatsPage.jsx
@@ -1,19 +1,7 @@
 /* eslint-disable react/prop-types */
-// import {
-//   MultiChatSocket,
-//   MultiChatWindow,
-//   useMultiChatLogic,
-// } from "react-chat-engine-advanced";
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 const ChatsPage = (props) => {
-  //   const chatprop = useMultiChatLogic(
-  //     "88cc7c3f-46c7-4409-afff-d96eaf2886c0",
-  //     props.user.username,
-  //     props.user.secret
-  //   );
   return (
-    //   <MultiChatSocket {...chatprop} />
-    //   <MultiChatWindow {...chatprop} style={{ height: "100vh" }} />
     <div style={{ height: "100vh" }}>
       <PrettyChatWindow
         projectId="88cc7c3f-46c7-4409-afff-d96eaf2886c0"
